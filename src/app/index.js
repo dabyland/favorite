4abcb7e9eeb321c9df802cef584524a1650ef504
Danny Byland <Daniel.Byland@target.com>
2019-02-17 10:02:55 -0600

    Add check for no user_profile

--- src/app/index.js
+++ src/app/index.js
@@ -12,20 +12,23 @@ function signUp(form) {
 
 function authenticateUser(form) {
   const userProfile = JSON.parse(localStorage.getItem("user_profile"));
-  const usernameMatches = userProfile.username === form.username.value;
-  const passwordMatches = userProfile.password === form.password.value;
-
-  if (usernameMatches && passwordMatches) {
-    alert(`Welcome ${userProfile.username}!`);
-
-    window.location.href = "home/home.html";
-  } else if (usernameMatches && !passwordMatches) {
-    alert("The password entered is incorrect");
-  } else if (!usernameMatches && passwordMatches) {
-    alert("The username entered is incorrect");
-  } else {
-    alert("The information entered does not match any profile. Please try again");
+  
+  if (userProfile !== null) {
+    const usernameMatches = userProfile.username === form.username.value;
+    const passwordMatches = userProfile.password === form.password.value;
+  
+    if (usernameMatches && passwordMatches) {
+      alert(`Welcome ${userProfile.username}!`);
+      window.location.href = "home/home.html";
+    } else if (usernameMatches && !passwordMatches) {
+      alert("The password entered is incorrect");
+    } else if (!usernameMatches && passwordMatches) {
+      alert("The username entered is incorrect");
+    } else {
+      alert("The information entered does not match any profile. Please try again");
+    }
   }
+  alert("We cannot find an account matching the entered information. Please try signing up before logging in.")
   return false;
 }
 
